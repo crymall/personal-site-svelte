da8deee21086c7bdf1f7a779a22fb58e58e39693
crymall <reed.gaines@gmail.com>
2021-10-20 18:04:45 -0400

    disclaimer for deployed site

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,6 +3,7 @@
     <h1>Hi there.</h1>
     <p>My name is Reed Gaines, and I'm a web developer based in Brooklyn.</p>
     <p>Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.</p>
+    <p>(This website is under construction. What you're seeing above is <a href="https://cipsum.com/">Corporate Ipsum</a>.)</p>
   </div>
   <img class="me" src="/me.jpg" alt="Reed Gaines" />
 </div>
