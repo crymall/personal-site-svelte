513c265e2d60b9487714621dc1402463e6a22c34
crymall <reed.gaines@gmail.com>
2021-10-20 17:48:10 -0400

    configure deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,11 @@
 import sveltePreprocess from "svelte-preprocess";
+import adapter from '@sveltejs/adapter-netlify';
 /** @type {import('@sveltejs/kit').Config} */
 
 const config = {
 	preprocess: sveltePreprocess(),
 	kit: {
+		adapter: adapter(),
 		target: '#svelte'
 	}
 };
