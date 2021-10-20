bc2d6535c6353b52a25ddf48096882fb88c5e4fc
crymall <reed.gaines@gmail.com>
2021-10-20 13:03:48 -0400

    first commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,25 @@
+<script>
+  let darkMode = false;
+
+  const toggleDarkMode = () => {
+    darkMode = !darkMode;
+    const body = document.querySelector('body');
+    body.className = darkMode ? 'dark-mode' : '';
+  };
+</script>
+
+<nav class="topnav">
+  <div class="leftside">
+    <a href="/">home</a>
+    <a href="/about">about</a>
+  </div>
+  <button on:click={toggleDarkMode}>
+    toggle dark mode
+  </button>
+</nav>
+
+<slot></slot>
+
+<style lang="scss">
+  @import 'src/styles/layout.scss';
+</style>
